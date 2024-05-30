42f1f9c8a37df8fd2735f557f9fab9e1aa5adaf9
Eknow <foxyto.yt@gmail.com>
2024-05-30 11:03:41 -0600

    refactor(dataUsers):add  config to users data

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { themeSettings } from "theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { themeSettings } from "./theme";
+import Dashboard from "./scenes/Dashboard";
+import RequireAuth from "./middleware/RequireAuth";
+import RedirectIfAuthenticated from "./middleware/RedirectIfAuthenticated";
+import Login from "login";
 import Layout from "scenes/layout";
 
 function App() {
     const mode = useSelector((state) => state.global.mode);
     const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
     return (
         <div className="app">
             <BrowserRouter>
                 <ThemeProvider theme={theme}>
                     <CssBaseline />
                     <Routes>
-                        <Route element={<Layout />}>
-                            <Route
-                                path="/"
-                                element={<Navigate to="/dashboard" replace />}
-                            />
-                            <Route path="/dashboard" />
+                        <Route
+                            path="/"
+                            element={
+                                <RedirectIfAuthenticated>
+                                    <Login />
+                                </RedirectIfAuthenticated>
+                            }
+                        />
+                        <Route
+                            element={
+                                <RequireAuth>
+                                    <Layout />
+                                </RequireAuth>
+                            }>
+                            <Route path="/dashboard" element={<Dashboard />} />
                             {/* <Route path="/products" element={<Products />} />
                             <Route path="/customers" element={<Customers />} />
                             <Route
@@ -36,8 +50,9 @@ function App() {
                             <Route
                                 path="/performance"
                                 element={<Performance />}
-                            />
-                            */}
+                            /> */}
+                            <Route path="/profile/:id" />{" "}
+                            {/* Añadimos la ruta del perfil */}
                         </Route>
                     </Routes>
                 </ThemeProvider>
---
+++ src/fakeData.js
@@ -0,0 +1,25 @@
+const users = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john@example.com",
+        password: "password123",
+        imageUrl: "https://randomuser.me/api/portraits/men/1.jpg",
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        email: "jane@example.com",
+        password: "password123",
+        imageUrl: "https://randomuser.me/api/portraits/women/2.jpg",
+    },
+    {
+        id: 3,
+        name: "Sam Wilson",
+        email: "sam@example.com",
+        password: "password123",
+        imageUrl: "https://randomuser.me/api/portraits/men/3.jpg",
+    },
+];
+
+export default users;
